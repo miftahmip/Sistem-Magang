81700b73dc730cc8da4dbd3d0ec53f765d04806c
Nabila 2018 <nbilardzkr20@gmail.com>
2025-04-21 20:50:53 +0700

    layoutpeserta

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    content: [
+       './views/*.{html,js}',
+       './views/KTU/*.{html,js}',
+       './views/*.{html,js}',
+        'node_modules/preline/dist/*.js',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [
+      // require('@tailwindcss/forms'),
+        require('preline/plugin'),
+    ],
+  }
